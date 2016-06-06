1bfc70b5753b370698c9c46ea0f59fdc990d77bc
Kris Bray <krisbray@krisco.net>
2016-06-06 11:10:25 -0600

    Initial Commit

---
+++ SwaggerTater/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SwaggerTater
+//
+//  Created by Kris on 2016-06-06.
+//  Copyright © 2016 KriscoDesigns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
